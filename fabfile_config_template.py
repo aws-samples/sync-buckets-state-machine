a9a47f9a081b375bd4f8752970143bedb2a6f6ee
Constantin Gonzalez <glez@amazon.de>
2017-04-24 15:23:35 +0200

    Fixed a typo.

--- fabfile_config_template.py
+++ fabfile_config_template.py
@@ -1,6 +1,6 @@
 #
 # Please customize the constants below and save as 'fabfile_config.py'.
 #
-USER_EMAIL = 'yoir_email@domain.com'  # This is used to generate a unique world-wide Amazon S3 bucket name.
+USER_EMAIL = 'your_email@domain.com'  # This is used to generate a unique world-wide Amazon S3 bucket name.
 AWS_DEFAULT_PROFILE = 'default'       # May be omitted, in which case the environment variable of same name is used.
 AWS_DEFAULT_REGION = 'us-east-1'      # May be omitted, in which case the environment variable of same name is used.
